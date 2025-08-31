6911e482a316724b16a41ba13f9a52149dd47c8c
@vishnuBhai16 <debeloperweb@gmail.com>
2025-08-31 21:52:37 +0530

    Added Calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+let display = document.getElementById("display");
+function appendValue(value) {
+  display.value += value;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculateResult() {
+  try {
+    display.value = eval(display.value); // calculates the expression
+  } catch {
+    display.value = "Error";
+  }
+}
